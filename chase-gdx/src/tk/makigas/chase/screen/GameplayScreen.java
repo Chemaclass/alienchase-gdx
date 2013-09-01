bed6b98f2dbc049f85cf1ccf31df1d609ddb8aea
Dani Rodríguez <danirod@outlook.com>
2013-09-01 23:32:40 +0200

    Refactoriza la pantalla principal de juego
    Cambios realizados:
    
    * Se ha documentado la mayoría del código fuente que debiera
      ser documentado. Cada método indica qué hay que hacer, y cada
      operación medianamente compleja lleva comentarios que indica
      qué es lo que hace.
    
    * Para intentar simplificar el código, se han abstraido algunas
      partes complejas de la inicialización (método show()) en otros
      submétodos privados que son invocados.

--- chase-gdx/src/tk/makigas/chase/screen/GameplayScreen.java
+++ chase-gdx/src/tk/makigas/chase/screen/GameplayScreen.java
@@ -36,7 +36,7 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 /**
- * Pantalla de juego principal.
+ * Pantalla de juego principal. Aquí es donde se desarrolla la trama.
  * 
  * @author danirod
  */
@@ -63,8 +63,10 @@ public class GameplayScreen extends AbstractScreen {
 	/** Puntuación */
 	private PuntuacionActor puntuacion;
 	
+	/** Lista de aliens cargados en el juego. */
 	private List<AlienActor> aliens;
 	
+	/** Lista de balas cargadas en el juego. */
 	private List<BulletActor> bullets;
 	
 	public GameplayScreen(AlienChase game) {
@@ -73,6 +75,7 @@ public class GameplayScreen extends AbstractScreen {
 	
 	@Override
 	public void show() {
+		// (Re)inicializamos las listas de entidades cargadas en el juego.
 		aliens = new ArrayList<AlienActor>();
 		bullets = new ArrayList<BulletActor>();
 		
@@ -82,7 +85,24 @@ public class GameplayScreen extends AbstractScreen {
 		stage = new Stage(width, height, true, game.sb);
 		Gdx.input.setInputProcessor(stage);
 		
-		// Crear fondo.
+		initActors(); // inicializamos todos los actores en el escenario
+		initInput(); // inicializamos el sistema de entrada
+
+		// Finalmente inicializamos el contador de tiempo. Esto sólo
+		// ajusta el tiempo que queda para que el primer alien haga spawn.
+		timer = 2 + (float) Math.random();
+	}
+
+	/**
+	 * Inicializa los actores. Se crean todas las naves y demás elementos
+	 * que deban añadirse al escenario. El orden en el que se hacen es
+	 * importante para que se muestren correctamente unos delante de otros.
+	 */
+	private void initActors() {
+		// Crear fondo. Para crear el fondo se usa una imagen, que es un
+		// elemento de scene2d-ui que permite crear imágenes simples.
+		// Para lo que lo queremos, nos vale y nos sobra. Hay que añadirlo
+		// en primer lugar para que quede en el fondo en todo momento.
 		Image imgFondo = new Image(AlienChase.MANAGER.get("fondo.png", Texture.class));
 		imgFondo.setFillParent(true);
 		stage.addActor(imgFondo);
@@ -105,13 +125,25 @@ public class GameplayScreen extends AbstractScreen {
 		stage.addActor(vidaNave);
 		stage.addActor(vidaEscudo);
 		
-		// Creamos los sistemas de entrada. En escritorio tendremos que usar
-		// un listener que lo hace todo, mientras que para Android tendremos
-		// que usar tres botones asociados cada uno a algo.
-		if(Gdx.app.getType() == ApplicationType.Desktop) {
+		// La puntuación es un actor que muestra un string en pantalla.
+		// TODO: Usar una tipografía propia más chula que la Arial...
+		puntuacion = new PuntuacionActor(new BitmapFont());
+		puntuacion.setPosition(10, stage.getHeight() - 10);
+		puntuacion.puntuacion = 0;
+		stage.addActor(puntuacion);
+	}
+
+	/**
+	 * Inicializa los sistemas de entrada. En escritorio se usa un listener
+	 * capaz de realizar todas las acciones a la primera. En cambio, en
+	 * Android es necesario procesar estas acciones usando pads que habrá que
+	 * agregar al escenario.
+	 */
+	private void initInput() {
+		if(Gdx.app.getType() == ApplicationType.Desktop) {			// DESKTOP
 			stage.setKeyboardFocus(nave); // damos foco a nave.
 			nave.addListener(new InputDesktopListener(nave, stage, bullets));
-		} else if(Gdx.app.getType() == ApplicationType.Android) {
+		} else if(Gdx.app.getType() == ApplicationType.Android) { 	// ANDROID
 			// Creamos los pads.
 			padArriba = new PadActor(0, 0);
 			padAbajo = new PadActor(1, 0);
@@ -132,72 +164,94 @@ public class GameplayScreen extends AbstractScreen {
 			stage.addActor(padAbajo);
 			stage.addActor(padShoot);
 		}
-		
-		puntuacion = new PuntuacionActor(new BitmapFont());
-		puntuacion.setPosition(10, stage.getHeight() - 10);
-		puntuacion.puntuacion = 0;
-		stage.addActor(puntuacion);
-		
-		// Finalmente inicializamos el contador de tiempo.
-		timer = 2 + (float) Math.random();
 	}
 
 	@Override
 	public void render(float delta) {
+		// Limpieza de la pantalla.
 		Gdx.gl.glClear(GL11.GL_COLOR_BUFFER_BIT);
 		
+		// Actualizamos.
 		stage.act();
 		timer -= delta;
-		if(timer < 0)		
+		if(timer < 0) // hora de hacer spawn	
 			dispararAlien();
 		comprobarListas();
 		comprobarColisiones();
 		stage.draw();
 	}
 	
+	/**
+	 * Atiende a cada una de las entidades de las listas. Este método
+	 * comprobará si por ejemplo es necesario elimnar aliens o balas que
+	 * hayan sobrepasado los límites de la pantalla.
+	 * 
+	 * A la hora de eliminar una entidad es necesario llamar a dos métodos
+	 * remove. En primer lugar, es necesario eliminar el actor del escenario.
+	 * Para ello se usa el método remove() de la clase Actor. Hecho eso,
+	 * hace falta eliminar a la entidad de la lista dinámica en la que esté
+	 * introducida.
+	 */
 	private void comprobarListas() {
+		// Busca aliens que hayan salido por el lazo izquierdo de la pantalla
 		for(int i = 0; i < aliens.size(); i++) {
 			if(aliens.get(i).getRight() < 0) {
-				aliens.get(i).remove();
-				aliens.remove(i);
+				aliens.get(i).remove(); // fuera del escenario
+				aliens.remove(i); // fuera de la lista dinámica
+				
+				// Es necesario también quitarle vida al escudo.
+				// Cada impacto resta un 40% de vida al escudo.
 				if(escudo.getHealth() > 0.4f) {
 					escudo.sumHealth(-0.4f);
 					AlienChase.MANAGER.get("hit.ogg", Sound.class).play();
 				} else {
+					// Si el escudo ya tenía menos de un 40% de vida,
+					// entonces el ataque a la tierra es inminente.
 					game.setScreen(game.GAMEOVER);
 				}
 			}
 		}
 		
+		// Busca balas que hayan salido por el lado derecho de la pantalla.
 		for(int i = 0; i < bullets.size(); i++) {
 			if(bullets.get(i).getX() > stage.getWidth()) {
-				bullets.get(i).remove();
-				bullets.remove(i);
+				bullets.get(i).remove(); // fuera del escenario
+				bullets.remove(i); // fuera de la lista dinámica
 			}
 		}
 	}
 	
+	/**
+	 * Comprueba el estado de las colisiones entre las distintas entidades
+	 * del juego usando un sistema de bounding-boxes. Cada entidad tiene
+	 * su bounding-box asociado y las colisiones se comprueban de forma
+	 * geométrica.
+	 */
 	private void comprobarColisiones() {
 		AlienActor alien;
+		
 		for(int i = 0; i < aliens.size(); i++) {
 			alien = aliens.get(i);
-			if(alien.bb.overlaps(nave.bb)) {
+			if(alien.bb.overlaps(nave.bb)) { // ¿este alien choca con nave?
 				// Colisión alien-nave.
-				aliens.get(i).remove();
-				aliens.remove(i);
+				aliens.get(i).remove(); // eliminar del escenario
+				aliens.remove(i); // eliminar de la lista dinámica
 				nave.sumHealth(-0.4f);
 				AlienChase.MANAGER.get("hit.ogg", Sound.class).play();
 				if(nave.getHealth() <= 0) {
+					// si no hay vida en la nave, entonces se acabó la partida
 					game.setScreen(game.GAMEOVER);
 				}
-			} else {
+			} else {	// ya sabemos que este alien no ha impactado con la
+						// nave, ahora comprobamos si ha chocado con una bala.
+						// probablemente este no es el sistema más eficiente...
 				for(int j = 0; j < bullets.size(); j++) {
 					if(bullets.get(j).bb.overlaps(alien.bb)) {
 						// Colisión alien-bala.
-						aliens.get(i).remove();
-						aliens.remove(i);
-						bullets.get(j).remove();
-						bullets.remove(j);
+						aliens.get(i).remove(); // fuera del escenario
+						aliens.remove(i); // fuera de la lista dinámica
+						bullets.get(j).remove(); // fuera del escenario
+						bullets.remove(j); // fuera de la lista dinámica
 						AlienChase.MANAGER.get("explosion.ogg", Sound.class).play();
 						puntuacion.puntuacion++;
 					}
@@ -206,20 +260,31 @@ public class GameplayScreen extends AbstractScreen {
 		}
 	}
 
+	/**
+	 * Genera un nuevo alien en el escenario. Este método es invocado de
+	 * forma automática cuando el temporizador indica que es hora de disparar
+	 * un nuevo alien.
+	 */
 	private void dispararAlien() {
 		AlienActor alien = new AlienActor();
+		
+		// Le damos una posición en al pantalla a él y a su bounding box.
 		alien.setPosition(stage.getWidth(), 0.1f * stage.getHeight() + 
 				0.8f * stage.getHeight() * (float) Math.random());
 		alien.bb.x = alien.getX();
 		alien.bb.y = alien.getY();
+		
 		stage.addActor(alien);
 		aliens.add(alien);
+		
+		// Hay que actualizar el contador. Es aquí donde realmente
+		// se indica cada cuánto tiempo se disparará un alien nuevo.
 		timer = 2 + (float) Math.random();
 	}
 
 	@Override
 	public void hide() {
-		Gdx.input.setInputProcessor(null);
+		Gdx.input.setInputProcessor(null); // desbloqueamos la entrada
 	}
 
 	@Override
@@ -227,9 +292,17 @@ public class GameplayScreen extends AbstractScreen {
 		stage.dispose();
 	}
 
+	/**
+	 * En un principio este método no deberá ser llamado en escritorio,
+	 * puesto que el juego está configurado como no redimensionable.
+	 * En teoría tampoco debería llamarse en Android pero nunca se sabe.
+	 */
 	@Override
 	public void resize(int width, int height) {
+		// Actualizamos el tamaño del escenario.
 		stage.setViewport(640, 360, true);
+		
+		// Reposicionamos los actores cuya posición dependa del stage.
 		vidaNave.setPosition(stage.getWidth() - 150, stage.getHeight() - 20);
 		vidaEscudo.setPosition(stage.getWidth() - 150, stage.getHeight() - 28);
 		puntuacion.setPosition(10, stage.getHeight() - 10);
